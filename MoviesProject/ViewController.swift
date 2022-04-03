e71e8b1ba8800eb7975c3d15097c9c8589b86b1e
Muhammad Ahmad Munir <ahmadchaudhary06@gmail.com>
2022-04-03 11:15:35 +0500

    Initial Commit

---
+++ MoviesProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesProject
+//
+//  Created by ahmad on 03/04/2022.
+//  Copyright © 2022 ahmad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
